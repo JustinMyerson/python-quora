50114d8b54c241e6e3eee187c1d3184d60f34928
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-06-13 11:42:39 +0200

    Response statuses added

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+items = {"foo": "The Foo Wrestlers"}
 
-@app.get("/")
+
+@app.get("/", status_code=200)
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/items/", status_code=201)
+async def create_item(name: str):
+    return {"name": name}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item": items[item_id]}
