5164dafa69b79def4eda23308a365b51d5ea0476
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-06-13 14:51:30 +0200

    Add register user

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import email
 from fastapi import FastAPI, APIRouter
+
+from user import User
+from user_data import USERS
 
 app = FastAPI(openapi_url="/openapi.json")
 api_router = APIRouter()
@@ -8,5 +12,17 @@ api_router = APIRouter()
 async def root():
     return {"message": "Hello User"}
 
+
+@app.post("/auth/register", status_code=201, response_model=User)
+async def register_user(*, userCreated: User) -> dict:
+    user_entry = User(
+        email=user_email,
+        firstName=user_first_name,
+        lastName=user_last_name,
+        password=user_password,
+    )
+    USERS.append(user_entry.dict())
+
+    return user_entry
 
 app.include_router(api_router)
---
+++ user_data.py
@@ -0,0 +1,3 @@
+USERS = [{"email": "jamyerson@gmail.com", "firstName": "Justin",
+          "lastName": "Myerson", "password": "password"}, {"email": "lamyerson@gmail.com", "firstName": "Lance",
+                                                           "lastName": "Myerson", "password": "password1"}]
