5db1de0dc1cf14f7765aadc9b073c1abae0a396e
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-06-17 13:58:47 +0200

    Add user to DB

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import psycopg2
 
 from config import config
-from connect_db import connect
 from fastapi import FastAPI, APIRouter, HTTPException
 
 import bcrypt
@@ -13,10 +12,41 @@ from user_data import USERS
 app = FastAPI(openapi_url="/openapi.json")
 api_router = APIRouter()
 
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
-# @app.get("/", status_code=200)
-# def root():
-#     return {"message": "Hello User"}
+try:
+    # read connection parameters
+    params = config()
+
+    # connect to the PostgreSQL server
+    print('Connecting to the PostgreSQL database...')
+    conn = psycopg2.connect(**params)
+
+    # create a cursor
+    cur = conn.cursor()
+
+    # execute a statement
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+
+    # close the communication with the PostgreSQL
+    cur.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+    print("ERROR")
+finally:
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
+
+
+@app.get("/", status_code=200)
+def root():
+    return {"message": "Hello User"}
 
 
 # @api_router.get("/user/{user_email}", status_code=200, response_model=User)
@@ -30,13 +60,8 @@ api_router = APIRouter()
 
 def hash_password(password: str):
     bytePwd = password.encode('utf-8')
-    # Generate salt
-    mySalt = bcrypt.gensalt()
-    # Hash password
-    hash = bcrypt.hashpw(bytePwd, mySalt)
-
-
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    hashed_pw = bcrypt.hashpw(bytePwd, bcrypt.gensalt())
+    return hashed_pw
 
 
 def check_email_valid(email):
@@ -47,24 +72,23 @@ def check_email_valid(email):
 
 
 @api_router.post("/auth/register/", status_code=201, response_model=User)
-def register_user(*, userCreated: User) -> dict:
-    email = ""
-    if check_email_valid(userCreated.email):
-        email = userCreated.email
-        user_entry = User(
-            email=email,
-            firstName=userCreated.firstName,
-            lastName=userCreated.lastName,
-            # Ensure that the password is stored in a hashed format
-            password=hash_password(userCreated.password),
-        )
-    USERS.append(user_entry.dict())
-    print(USERS)
-
-    return user_entry
+def add_user_to_db(email, firstName, lastName, password):
+    if check_email_valid(email):
+        if (len(password) >= 8):
+            conn = psycopg2.connect(**params)
+            # create a cursor
+            cur = conn.cursor()
+            query = 'INSERT INTO users(email, firstName, lastName, password) VALUES (%s, %s, %s, %s)'
+            cur.execute(query, (email, firstName,
+                        lastName, hash_password(password)))
+            conn.commit()
+            print("Records created successfully")
+            cur.close()
+            conn.close()
 
 
 app.include_router(api_router)
 
 if __name__ == '__main__':
-    connect()
+    add_user_to_db('test@gmail.com', 'John', 'Doe', 'Absbsbsbsbsbs')
+    print(len(hash_password("Absbsbsbsbsbs")))
